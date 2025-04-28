a48c8cdb2506cbb4cddafe3085bc041366ccfa5f
Nathan Walker <walkerrunpdx@gmail.com>
2025-04-28 14:57:33 -0700

    fix: config (#1)

--- rspack.config.ts
+++ rspack.config.ts
@@ -1,4 +1,9 @@
 import { createConfig } from '@nx/angular-rspack';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default createConfig({
     options: {
